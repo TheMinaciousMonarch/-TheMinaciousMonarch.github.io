ddcdc95a547f4df5b5826f78df2e8afd05549556
Brandon Scott Dye <81925111+TheMinaciousMonarch@users.noreply.github.com>
2023-12-06 10:49:19 -0600

    Quartz sync: Dec 6, 2023, 10:49 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://github.com/TheMinaciousMonarch/TheMinaciousMonarch.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
